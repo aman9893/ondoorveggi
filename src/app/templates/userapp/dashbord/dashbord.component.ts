4b73e8828d615fc29d3c2d907aeb2d286e53b7a7
aman9893 <aman9893jain@gmail.com>
2025-01-26 16:27:38 +0530

    add

--- src/app/templates/userapp/dashbord/dashbord.component.ts
+++ src/app/templates/userapp/dashbord/dashbord.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+import { AuthService } from '../../auth/auth.service';
+import { DataService } from '../../auth/service/data.service';
 register();
 @Component({
   selector: 'app-dashbord-user',
@@ -10,53 +12,47 @@ register();
 })
 export class DashbordComponentUser implements OnInit{
   menulist: any;
-
+  public appPages = [
+    {
+      title: "Profile",
+      url: "/userprofile",
+      icon: "person-circle"
+    },
+    {
+      title: "Orders",
+      url: "/Orders",
+      icon: "list-circle"
+    },
+    {
+      title: "Cart",
+      url: "/cart",
+      icon: "cart"
+    },
+    {
+      title: "Addresses",
+      url: "/Addresses",
+      icon: "location"
+    },
+    {
+      title: "Customer Support",
+      url: "/help",
+      icon: "headset"
+    },
+  
+  ];
+  public selectedIndex = 0;
+ constructor(public dataService: DataService,public authService: AuthService, ){}
   ngOnInit(): void {
-      this.menulistData();
-      
+      const path = window.location.pathname.split("folder/")[1];
+      if (path !== undefined) {
+        this.selectedIndex = this.appPages.findIndex(
+          page => page.title.toLowerCase() === path.toLowerCase()
+        );
+      }
+  }
+  logout() {
+    this.authService.logout();
   }
 
-  menulistData(){
-    this.menulist = [
-      {
-        title: 'Home',
-        icon: 'home',
-        link: '/',
-        color: '##000'
-      },
-      {
-        title: 'Profile',
-        icon: 'account_circle',
-        link: '/userprofile',
-        color: '##000'
-      },
-    
-      {
-        title: 'Orders',
-        icon: 'store_mall_directory',
-        link: '/Orders',
-        color: '##000'
-      },
-      {
-        title: 'Cart',
-        icon: 'add_shopping_cart',
-        link: '/cart',
-        color: '##000'
-      },
-      {
-        title: 'Addresses',
-        icon: 'location_on',
-        link: '/Addresses',
-        color: '##000'
-      },
-    
-      {
-        title: 'Customer Support',
-        icon: 'headset_mic',
-        link: '/help',
-        color: '##000'
-      },
-    ]  
-  }
   }
  
